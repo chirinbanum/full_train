fe98ed0be2e9fa99e46ad71a1fad80592800ace1
chirinbanum <chirinbanu2004@gmail.com>
2024-12-31 09:17:14 +0530

    Adding kec

--- fullstack_project/src/Components/ClassComponents/ClassCompEg.jsx
+++
@@ -1,9 +0,0 @@
-class ClassEg{
-    a=10
-    methodEg=()=>{
-        console.log("Welcome to OOPs");
-    }
-}
-obj=new ClassEg()
-console.log(obj.a);
-console.log(obj.methodEg())
--- fullstack_project/src/Components/FunctionalComponents/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-var NavBar = () => {
-    return <h1>This is Navigation Bar</h1>;
-};
-
-export default NavBar;
