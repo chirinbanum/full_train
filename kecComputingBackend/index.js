f52403ac0b61ecabed12dec3fe30788d5835db47
chirinbanum <chirinbanu2004@gmail.com>
2025-01-04 07:56:11 +0530

    Adding the CRUD

--- kecComputingBackend/index.js
+++ kecComputingBackend/index.js
@@ -78,6 +78,66 @@ app.post('/login',async(req,res)=>{
       
 })
 
+app.post("/update", async (req, res) => {
+    var { firstName, updatedata } = req.body;
+    console.log(req.body);
+    try {
+      var updateUser = await User.updateOne(
+        { firstName: firstName },
+        { $set: updatedata }
+      );
+      console.log(updateUser);
+      if (updateUser.matchedCount > 0) {
+        res.json({ message: "updated successfully...", isUpdated: true });
+        console.log("updated user successfully...");
+      } else {
+        console.log("user not found..");
+      }
+    } catch (err) {
+      res.json({ message: "invalid data...", isUpdated: false });
+  
+      console.log("invalid data..");
+    }
+  });
+  app.post("/delete", async (req, res) => {
+    var { email } = req.body;
+    console.log(req.body);
+    try {
+      var deletedUser = await User.deleteOne({ email: email });
+      console.log(deletedUser);
+      if (deletedUser.deletedCount > 0) {
+        res.json({
+          message: "deleted successfully...",
+          isDeleted: true,
+        });
+      } else {
+        console.log("user not found..");
+      }
+    } catch (err) {
+      res.json({ message: "invalid data..", isDeleted: false });
+      console.log("error in deleting");
+    }
+  });
+  
+  app.get("/find/:id", async (req, res) => {
+    var { id } = req.params;
+    console.log(req.params);
+    try {
+      var existingUser = await User.findOne({_id:id});
+      console.log(existingUser);
+      console.log(existingUser);
+      if (existingUser) {
+          res.json({ message: "Display successful.."});
+        } else {
+          res.json({ message: "invalid credentials"});
+        }
+      } 
+     catch (err) {
+      console.log("failed to fetch");
+    }
+  });
+  
+
 app.listen(PORT, () => { 
     console.log(`Backend sever started\nUrl:http://localhost:${PORT}`);
 
